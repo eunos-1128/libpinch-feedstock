9f2ff517ad1666c43a0209dae7dd34861f11960b
UENO, M. <eunosium-1128@live.jp>
2026-06-03 00:30:48 +0900

    Disable PINCH_BUILD_TESTS during build
    Disable tests for pinch build on Linux target platform.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
 if [[ ${target_platform} == "linux-"* ]]; then
-    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DPINCH_BUILD_TESTS=ON"
+    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DPINCH_BUILD_TESTS=OFF"
 
     if [[ ${build_platform} != ${target_platform} ]]; then
         CMAKE_ARGS="${CMAKE_ARGS} -DTEST_STD_CHRONO_FROM_STREAM_R=ON"
@@ -15,10 +15,7 @@ elif [[ ${target_platform} == "osx-"* ]]; then
 fi
 
 cmake -S . -B build ${CMAKE_ARGS}
-
 cmake --build build --parallel ${CPU_COUNT}
-
-ctest -V --test-dir build
 cmake --install build
 
 install -m 644 include/pinch/asio.hpp ${PREFIX}/include/pinch/asio.hpp
