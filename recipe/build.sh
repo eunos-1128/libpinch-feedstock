aea8b8143a433cac922807885bcbffa3923859fa
UENO, M. <eunosium-1128@live.jp>
2026-06-03 00:39:28 +0900

    Disable building examples and tests in build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,14 @@ set -exo pipefail
 export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
 if [[ ${target_platform} == "linux-"* ]]; then
-    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DPINCH_BUILD_TESTS=OFF"
+    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DPINCH_BUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF"
 
     if [[ ${build_platform} != ${target_platform} ]]; then
         CMAKE_ARGS="${CMAKE_ARGS} -DTEST_STD_CHRONO_FROM_STREAM_R=ON"
     fi
 elif [[ ${target_platform} == "osx-"* ]]; then
     # Test code is not written for mac
-    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON"
+    CMAKE_ARGS="${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON -DPINCH_BUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF"
 fi
 
 cmake -S . -B build ${CMAKE_ARGS}
